556412c1a6f4b3d79c5ad804b7c515b4741f8865
m4t1ce <m4t1ce@gmail.com>
2022-03-04 17:01:27 +0100

    added meta and desc tag

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { About } from "../components/About";
 import { Navigation } from "../components/comps/Navigation";
 import { Contact } from "../components/Contact";
@@ -12,6 +13,13 @@ export default function Home() {
 				name="viewport"
 				content="width=device-width, initial-scale=1"
 			></meta>
+			<meta
+				name="description"
+				content="Fachowe usługi elektryczne w Warszawa i okolicach. Naprawy awarii, kompleksowe uruchomienia oraz instalacje w zakresie elektryki. Dla indywidualnych i biznesów."
+				// content="Fachowe usługi elektryczne w Warszawa i okolicach. Naprawy awarii, kompleksowe uruchomienia, instalacje w zakresie elektryki."
+			/>
+			<link rel="shortcut icon" href="/miernik.svg"></link>
+			<title>JPDsystem | Elektryk</title>
 			<Navigation />
 			<Logo />
 			<About />
