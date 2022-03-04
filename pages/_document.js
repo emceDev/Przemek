347932aecc6845e27248aa6fbd275a6d87c0348c
m4t1ce <m4t1ce@gmail.com>
2022-03-04 17:11:17 +0100

    implemented lang to html tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,19 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
 	render() {
 		return (
-			<Html>
+			<Html lang="pl-PL">
 				<Head>
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1"
+					></meta>
+					<meta
+						name="description"
+						content="Fachowe usługi elektryczne w Warszawa i okolicach. Naprawy awarii, kompleksowe uruchomienia oraz instalacje w zakresie elektryki. Dla indywidualnych i biznesów."
+						// content="Fachowe usługi elektryczne w Warszawa i okolicach. Naprawy awarii, kompleksowe uruchomienia, instalacje w zakresie elektryki."
+					/>
+					<link rel="shortcut icon" href="/miernik.svg"></link>
+					<title>JPDsystem | Elektryk</title>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
 					<link
--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,6 @@ import { Projects } from "../components/Projects";
 export default function Home() {
 	return (
 		<div className="Home">
-			<meta
-				name="viewport"
-				content="width=device-width, initial-scale=1"
-			></meta>
-			<meta
-				name="description"
-				content="Fachowe usługi elektryczne w Warszawa i okolicach. Naprawy awarii, kompleksowe uruchomienia oraz instalacje w zakresie elektryki. Dla indywidualnych i biznesów."
-				// content="Fachowe usługi elektryczne w Warszawa i okolicach. Naprawy awarii, kompleksowe uruchomienia, instalacje w zakresie elektryki."
-			/>
-			<link rel="shortcut icon" href="/miernik.svg"></link>
-			<title>JPDsystem | Elektryk</title>
 			<Navigation />
 			<Logo />
 			<About />
